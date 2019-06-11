3d7a06a46ed56826b22cb2e1178af439a2f4986e
Abd3lrhm4n <abdelrhmanelsaid681@gmail.com>
2019-06-11 10:15:15 +0200

    change the nav style and set a logo

--- UserInfo.py
+++ UserInfo.py
@@ -10,8 +10,10 @@ def UserInfoAnalysis(info, pw):
     info = info.lower()
     pw = pw.lower()
     
+    # found words list
     found = list()
 
+    # word which found
     word = ""
 
     i = 0
@@ -24,48 +26,67 @@ def UserInfoAnalysis(info, pw):
             # p = pw[j]
             # w = info[i]
 
+            # if the end of word start from began again
             if i == len(info):
 
                 i = 0
                 j += 1
 
+                # if the end of word
                 if j == len(pw):
                         
+                    # and word found
                     if len(word) > 0:
                         
+                        # append found word in list
                         found.append(word)
 
                     break
 
+            # if the word not equal move to next
             if pw[j] != info[i]:
 
                 i += 1
 
+                # and if there's a word found
                 if len(word) > 0:
 
+                    # append it
                     found.append(word)
 
+                    # then clear
                     word = ""
 
+                    # start over again
                     i = 0
 
             else:
 
+                # append this char to word
                 word += info[i]
 
+                # then move next
                 i += 1
                 j += 1
 
+        # the end of word
         if j == len(pw):
             
+            # and there's a word
             if len(word) > 0:
                 
+                # append it
                 found.append(word)
 
             break
         
+    # if found list not empty
     if len(found):  
+
+        # get the longest word and return it
         return max(found, key=len)
+
+    # if nothing return null
     else:
         return ''
 
