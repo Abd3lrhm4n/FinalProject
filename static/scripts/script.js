6a86b62610cee7b5193cc6e9b744c324c0487ccd
Abd3lrhm4n <abdelrhmanelsaid681@gmail.com>
2019-06-11 14:12:52 +0200

    comment the js and remove unwanted variables and set the font locally

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -16,62 +16,62 @@ $('document').ready(function(){
 
     PasswordValidator.setup('password');
 
-    $("#password").on("keyup", function(){
+    // $("#password").on("keyup", function(){
 
-        let v = $("#password").val();
+    //     let v = $("#password").val();
 
-        // check pw contain number
-        if(v.match("[0-9]")){
-            $("li:contains('Must contain a number.')").addClass("valid");
-            $("li:contains('Must contain a number.')").removeClass("invalid");
-        }
-        else{
-            $("li:contains('Must contain a number.')").addClass("invalid");
-            $("li:contains('Must contain a number.')").removeClass("valid");
-        }
+    //     // check pw contain number
+    //     if(v.match("[0-9]")){
+    //         $("li:contains('Must contain a number.')").addClass("valid");
+    //         $("li:contains('Must contain a number.')").removeClass("invalid");
+    //     }
+    //     else{
+    //         $("li:contains('Must contain a number.')").addClass("invalid");
+    //         $("li:contains('Must contain a number.')").removeClass("valid");
+    //     }
 
-        // check pw length > 8
-        if(v.length > 7){
-            $("li:contains('Must be at least 8 characters long.')").addClass("valid");
-            $("li:contains('Must be at least 8 characters long.')").removeClass("invalid");
-        }
-        else{
-            $("li:contains('Must be at least 8 characters long.')").addClass("invalid");
-            $("li:contains('Must be at least 8 characters long.')").removeClass("valid");
-        }
+    //     // check pw length > 8
+    //     if(v.length > 7){
+    //         $("li:contains('Must be at least 8 characters long.')").addClass("valid");
+    //         $("li:contains('Must be at least 8 characters long.')").removeClass("invalid");
+    //     }
+    //     else{
+    //         $("li:contains('Must be at least 8 characters long.')").addClass("invalid");
+    //         $("li:contains('Must be at least 8 characters long.')").removeClass("valid");
+    //     }
         
-        // check pw contain lowercase
-        if(v.match("[a-z]")){
-            $("li:contains('Must contain a lowercase letter.')").addClass("valid");
-            $("li:contains('Must contain a lowercase letter.')").removeClass("invalid");
-        }
-        else{
-            $("li:contains('Must contain a lowercase letter.')").addClass("invalid");
-            $("li:contains('Must contain a lowercase letter.')").removeClass("valid");
+    //     // check pw contain lowercase
+    //     if(v.match("[a-z]")){
+    //         $("li:contains('Must contain a lowercase letter.')").addClass("valid");
+    //         $("li:contains('Must contain a lowercase letter.')").removeClass("invalid");
+    //     }
+    //     else{
+    //         $("li:contains('Must contain a lowercase letter.')").addClass("invalid");
+    //         $("li:contains('Must contain a lowercase letter.')").removeClass("valid");
 
-        }
+    //     }
 
-        // check pw contain uppercase
-        if(v.match("[A-Z]")){
-            $("li:contains('Must contain an uppercase letter.')").addClass("valid");
-            $("li:contains('Must contain an uppercase letter.')").removeClass("invalid");
-        }
-        else{
-            $("li:contains('Must contain an uppercase letter.')").addClass("invalid");
-            $("li:contains('Must contain an uppercase letter.')").removeClass("valid");
-        }
+    //     // check pw contain uppercase
+    //     if(v.match("[A-Z]")){
+    //         $("li:contains('Must contain an uppercase letter.')").addClass("valid");
+    //         $("li:contains('Must contain an uppercase letter.')").removeClass("invalid");
+    //     }
+    //     else{
+    //         $("li:contains('Must contain an uppercase letter.')").addClass("invalid");
+    //         $("li:contains('Must contain an uppercase letter.')").removeClass("valid");
+    //     }
 
-        // check pw contain special character
-        if (v.match("[/~/!/@/#/$/%/^/&/*/(/)/_/-/=/|/?/>/<]")){
-            $("li:contains('special character.')").addClass("valid");
-            $("li:contains('special character.')").removeClass("invalid");
-        }
-        else{
-            $("li:contains('special character.')").addClass("invalid");
-            $("li:contains('special character.')").removeClass("valid");
-        }
+    //     // check pw contain special character
+    //     if (v.match("[/~/!/@/#/$/%/^/&/*/(/)/_/-/=/|/?/>/<]")){
+    //         $("li:contains('special character.')").addClass("valid");
+    //         $("li:contains('special character.')").removeClass("invalid");
+    //     }
+    //     else{
+    //         $("li:contains('special character.')").addClass("invalid");
+    //         $("li:contains('special character.')").removeClass("valid");
+    //     }
 
-    });
+    // });
 
     // $("#password").on("focus", function(){
     //     $("ul").addClass('focus');
